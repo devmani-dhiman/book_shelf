be733fae65ff5137be578b1d27e7188ce4edf904
devmani.dhiman <manidhi19@gmail.com>
2021-03-06 13:15:13 +0530

    Excel file created and populated

--- db.py
+++ db.py
@@ -1,10 +1,38 @@
 import sqlite3
+from sqlite3 import Error
+import pandas as pd
 
-con = sqlite3.connect("books.db")
+#_cursor = conn.cursor()
+#_cursor.execute('CREATE TABLE test (id, Book Name text, Author text, Genre text, Sub-category text, Status text, Summary text)')
 
-_cursor = con.cursor()
+def create_connection(db_file):
+    """Create a database connection to SQLite database"""
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+        print("Connection Successful")
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        if conn:
+            print("Function Executed Successfully")
+        else:
+            print("Unable to Connect")
 
-_cursor.execute('''CREATE TABLE books 
-                            (id, book name text, author text, category text, summary text, status text)''')
+conn = create_connection(r'D:\sqlite\db\test.db')
+df = pd.read_excel('Booklist.xlsx', header=0, index_col=0)
+print(df.head())
+#conn.cursor().execute("DROP table 'Test Table'")
+new_cols = []
+for column in df.columns:
+    cols = "".join(column.split())
+    new_cols.append(cols)
 
+print(new_cols)
+df.columns = new_cols
+print(df.head())
 
+df.to_sql(name='Test Table', con = conn, if_exists='append') 
+#('fail'- Default', 'replace'- To drop and create new)
