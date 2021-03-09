41fa5498beb92ed33b1bae50fc3eba2809060e15
devmani.dhiman <manidhi19@gmail.com>
2021-03-09 10:05:17 +0530

    Done

--- db.py
+++ db.py
@@ -5,6 +5,7 @@ import pandas as pd
 #_cursor = conn.cursor()
 #_cursor.execute('CREATE TABLE test (id, Book Name text, Author text, Genre text, Sub-category text, Status text, Summary text)')
 
+
 def create_connection(db_file):
     """Create a database connection to SQLite database"""
     conn = None
@@ -20,6 +21,35 @@ def create_connection(db_file):
         else:
             print("Unable to Connect")
 
+
+def get_all_itens(loc):
+    try:
+        conn = create_connection(loc = r'D:\sqlite\db\books.db')
+        _cursor = conn.cursor()
+        _cursor.execute("Select * from books")
+        rows = _cursor.fetchall()
+        conn.close()
+        return {"count": len(rows), "items": rows}
+    except Error as e:
+        print('Error: ', e)
+        return None
+
+def get_reading(loc = r'D:\sqlite\db\books.db'):
+    try:
+        conn = create_connection(loc)
+        _cursor = conn.cursor()
+        _cursor.execute("Select * from Books where status = 'C'")
+        rows = _cursor.fetchall()
+        return rows
+        conn.close()
+    except Error as e:
+        print('Error: ', e)
+        return None
+
+def get_current():
+    pass
+
+"""
 conn = create_connection(r'D:\sqlite\db\books.db')
 df = pd.read_excel('Booklist.xlsx', header=0, index_col=0)
 #print(df.head())
@@ -35,3 +65,4 @@ df.columns = new_cols
 
 df.to_sql(name='Books', con = conn, if_exists='append') 
 #('fail'- Default', 'replace'- To drop and create new)
+conn.close()"""
--- shelf.py
+++ shelf.py
@@ -1,16 +1,19 @@
+import streamlit as st
+import db
+
 current_reading = ['Rework']
 completed = ['The Kite Runner', 'The Psychology of Money', 'Atomic Habbits', 'Harry Potter Serires']
 
 want_to_read = ['Sapiens', 'Angels and Demons', 'Sapiens', 'No Rules Rules']
 
-import streamlit as st
 
 st.header("Welcome to my book shelf.")
 
 selected = st.selectbox("Select", ["Reading", "Competed", "Future Reads"])
 
 if selected == "Reading":
-    st.write(current_reading)
+    books = db.get_reading()
+    st.write(books) 
 
 elif selected == "Future Reads":
     st.write(want_to_read)
