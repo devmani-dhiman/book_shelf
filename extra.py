e21a853fd4de2a8f1eb4c19b4f65f0aa9f2899ba
devmani.dhiman <manidhi19@gmail.com>
2021-03-18 21:41:09 +0530

    sqlite files seprated into another folder Currently reading directly from excel file.

---
+++ extra.py
@@ -0,0 +1,33 @@
+import db
+import streamlit as st
+
+selected = st.selectbox(
+    "Select", ["All", "Reading", "Completed", "Future Reads"])
+
+def select_subCategory():
+    sCategory = db.get_subCategory()
+    sub_cat = []
+    for sc in sCategory:
+        sub_cat.append(sc[0])
+    subCategory = st.selectbox("Category", sub_cat)
+    return subCategory
+
+if selected == "Completed":
+    tag = "C"
+    subCat = select_subCategory()
+    completed = db.get_completed()
+    to_show = db.book_with_subCategory(subCat, tag)
+    st.write(to_show)
+
+elif selected == "Future Reads":
+    tag = "F"
+    want_to_read = db.get_want_to_read()
+    st.write(want_to_read)
+
+elif selected == "Reading":
+    completed = db.get_reading()
+    tag = "R"
+    st.write(completed)
+
+all = db.get_all_items(r'D:\sqlite\db\books.db')
+st.write(all)
--- shelf.py
+++ shelf.py
@@ -1,49 +1,35 @@
 import streamlit as st
-import db
+#import db
+import pandas as pd
 
 st.header("Welcome to my book shelf.")
 
 selected = st.selectbox(
     "Select", ["All", "Reading", "Completed", "Future Reads"])
 
-tag = ""
 
-
-def select_subCategory():
-    sCategory = db.get_subCategory()
-    sub_cat = []
-    for sc in sCategory:
-        sub_cat.append(sc[0])
-    subCategory = st.selectbox("Category", sub_cat)
-    return subCategory
-
+books = pd.read_excel('Booklist.xlsx', header=0)
 
 if selected == "Completed":
-    tag = "C"
-    subCat = select_subCategory()
-    completed = db.get_completed()
-    to_show = db.book_with_subCategory(subCat, tag)
-    st.write(to_show)
+    books_completed = books.loc[books["Status"] == "C"]
+    st.table(books_completed)
 
 
 elif selected == "Future Reads":
-    tag = "F"
-    want_to_read = db.get_want_to_read()
-    st.write(want_to_read)
+    books_future = books.loc[books["Status"] == "F"]
+    st.table(books_future)
 
 
 elif selected == "Reading":
-    completed = db.get_reading()
-    tag = "R"
-    st.write(completed)
+    books_reading = books.loc[books["Status"] == "R"]
+    st.table(books_reading)
 
 else:
-    all = db.get_all_items(r'D:\sqlite\db\books.db')
-    st.write(all)
+    st.table(books)
 
 
 st.markdown("<address>If you want to send your recommendation you can send it to <a href=\"mailto:devmani96@gmail.com\">devmani96@gmail.com</a>  &#128512</address>",
             unsafe_allow_html=True)
 
 
-st.markdown("If you want to charge your Vocabulary <a href=\"https://twitter.com/learningtoget19\">Click Here</a> &#9889", unsafe_allow_html=True)
+st.markdown("If you want to brush your Vocabulary <a href=\"https://twitter.com/learningtoget19\">Click Here</a> &#9889", unsafe_allow_html=True)
